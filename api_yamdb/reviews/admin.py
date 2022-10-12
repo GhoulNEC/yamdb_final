f14b89e47a6f9897315d8e619241b32192a9632c
Роман Евстафьев <ev-rog96@mail.ru>
2022-10-12 17:37:37 +0300

    add project files

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import Comment, Genre, Review, Title
+
+
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'score', 'author', 'title')
+    search_fields = ('title', 'author')
+    list_filter = ('score', 'text',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Review, ReviewsAdmin)
+admin.site.register(Title)
+admin.site.register(Comment)
+admin.site.register(Genre)
